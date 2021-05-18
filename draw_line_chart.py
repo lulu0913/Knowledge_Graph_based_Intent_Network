dc173cdb3fa34f0cc373d77ac20039f19181ca9e
lulu0913 <1029390056@qq.com>
2021-05-18 21:43:16 +0800

    draw line chart of recall and precision and HR and ndcg on two dataset

--- draw_line_chart.py
+++ draw_line_chart.py
@@ -1,7 +1,21 @@
 import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+
+def save_variable(v, filename):
+    f = open(filename, 'wb')
+    pickle.dump(v, f)
+    f.close()
+    return filename
+
+def load_variavle(filename):
+    f = open(filename, 'rb')
+    r = pickle.load(f)
+    f.close()
+    return r
 
 recall_alibaba, ndcg_alibaba, precision_alibaba, hit_ratio_alibaba = [], [], [], []
-recall_fm, ndcg_fm, precision_fm, hit_ratio_fm = [], [], [], []
+# recall_fm, ndcg_fm, precision_fm, hit_ratio_fm = [], [], [], []
 flag = 0
 f = open("./training_log/Alibaba-iFashion.txt", "r", encoding='UTF-8')
 line = f.readline()
@@ -25,33 +39,35 @@ while line:
 
 f.close()
 
+recall_fm = load_variavle('recall.txt')
+ndcg_fm = load_variavle('ndcg.txt')
+precision_fm = load_variavle('precision.txt')
+hit_ratio_fm = load_variavle('hit_ratio.txt')
+
+recall_fm = [i.tolist() for i in recall_fm]
+ndcg_fm = [i.tolist() for i in ndcg_fm]
+precision_fm = [i.tolist() for i in precision_fm]
+hit_ratio_fm = [i.tolist() for i in hit_ratio_fm]
+
+recall_fm = [i[0] for i in recall_fm]
+ndcg_fm = [i[0] for i in ndcg_fm]
+precision_fm = [i[0] for i in precision_fm]
+hit_ratio_fm = [i[0] for i in hit_ratio_fm]
+
 x1 = [1]
 x2 = [10*i+9 for i in range(21)]
 x = x1 + x2
-y1 = [87, 174, 225, 254]
-y2 = [24, 97, 202, 225]
-y3 = [110, 138, 177, 205]
-y4 = [95, 68, 83, 105]
 
-plt.title('扩散速度')  # 折线图标题
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示汉字
-plt.xlabel('时间')  # x轴标题
-plt.ylabel('差值')  # y轴标题
-plt.plot(x, y1, marker='o', markersize=3)  # 绘制折线图，添加数据点，设置点的大小
-plt.plot(x, y2, marker='o', markersize=3)
-plt.plot(x, y3, marker='o', markersize=3)
-plt.plot(x, y4, marker='o', markersize=3)
+plt.title('precision随训练次数的变化')  # 折线图标题
 
-for a, b in zip(x, y1):
-    plt.text(a, b, b, ha='center', va='bottom', fontsize=10)  # 设置数据标签位置及大小
-for a, b in zip(x, y2):
-    plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
-for a, b in zip(x, y3):
-    plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
-for a, b in zip(x, y4):
-    plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
+plt.xlabel('epochs')  # x轴标题
+plt.ylabel('precision')  # y轴标题
 
-plt.legend(['方案一', '方案二', '方案三', '方案四'])  # 设置折线名称
+plt.plot(x, precision_alibaba, label='Alibaba-iFashion')
+plt.plot(x, precision_fm, label='Last-FM')
+plt.legend()
+# plt.legend(['方案一', '方案二', '方案三', '方案四'])  # 设置折线名称
 
 plt.show()  # 显示折线图
 
